3432932b9a0d3f907a4c93c1e266d5a76b5a32a4
dashlw <daisha.walton@du.edu>
2016-11-17 15:06:04 -0700

    added nav bar and artist photos and header - debugged

--- index.js
+++ index.js
@@ -1,67 +1,92 @@
 $(document).ready(function() {
 
+    $('.materialboxed').materialbox();
+
 });
 
 var map = {};
+var circles = [];
 
-$('button').click(function() {
+$("#add").click(function() {
+    $("#responsive-menu").toggle(400);
+});
 
-        var artist = $('input').val().toLowerCase();
-        $('input').val('');
+$("#mainButton").click(function() {
 
-        // var events = [];
+    $('h3').text("")
 
-        $.get('https://galvanize-cors-proxy.herokuapp.com/http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=musicmap&date=all')
+    var artist = $('input').val().toLowerCase();
+    $('input').val('');
 
-        .then(function(data) {
-            var cityList = {}
-            for (var j = 0; j < data.length; j++) {
-                var city = data[j].venue.city;
+    $.get('https://galvanize-cors-proxy.herokuapp.com/http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=musicmap&date=all')
 
-                if (!cityList[city]) {
-                    cityList[city] = {
-                        center: {
-                            lat: data[j].venue.latitude,
-                            lng: data[j].venue.longitude
-                        },
-                        count: 1,
-                    };
-                } else {
-                    cityList[city].count++
-                } // end city count
-            }
+    .then(function(data) {
+        // console.log(data);
 
-            // console.log(cityList);
+        clearCircles()
 
-            for (var city in cityList) {
-                // Add the circle for this city to the map.
-                var cityCircle = new google.maps.Circle({
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: '#FF0000',
-                    fillOpacity: 0.35,
-                    map: map,
-                    center: cityList[city].center,
-                    radius: Math.sqrt(cityList[city].count) * 4000
-                });
-            } // end circle add
+        var artistImg = data[0].artists[0].image_url;
+        $('.materialboxed').attr('src', artistImg)
 
-            $('#err').hide();
+        var artistName = data[0].artists[0].name;
+        $('h3').append(artistName)
 
-        }).catch(function(error) {
-            $('#err').show();
-        })
-    }) // end button click function
+        // var windowContent =
 
-var chicago = {
-    lat: 41.878,
-    lng: -87.62
-}
+        var cityList = {}
+        for (var j = 0; j < data.length; j++) {
+            var city = data[j].venue.city;
+
+            if (!cityList[city]) {
+                cityList[city] = {
+                    center: {
+                        lat: data[j].venue.latitude,
+                        lng: data[j].venue.longitude
+                    },
+                    count: 1,
+                };
+            } else {
+                cityList[city].count++
+            } // end city count
+        }
+
+        for (var city in cityList) {
+            // Add the circle for this city to the map.
+            var cityCircle = new google.maps.Circle({
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#CE1483',
+                fillOpacity: 0.35,
+                map: map,
+                center: cityList[city].center,
+                radius: Math.sqrt(cityList[city].count) * 8000
+
+            });
+
+            circles.push(cityCircle)
+
+
+
+        } // end circle add
+
+        $('#err').hide();
+
+    }).catch(function(error) {
+        $('#err').show();
+    });
+
+}); // end button click function
+
 
 var denver = {
     lat: 39.7392,
     lng: -104.9903
+}
+
+var la = {
+    lat: 34.0522,
+    lng: -118.2437
 }
 
 var ny = {
@@ -74,26 +99,19 @@ var london = {
     lng: 0.1278
 }
 
-
-function initMap() {
-
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-        center: {
-            lat: 40.674,
-            lng: -104.945,
-        },
-        zoom: 5,
-    });
-} // end map function
-
-$('#chicago').on('click', function() {
-    map.setZoom(8);
-    map.setCenter(chicago);
-});
+var worldview = {
+    lat: 38.7223,
+    lng: -9.1393
+}
 
 $('#denver').on('click', function() {
     map.setZoom(8);
     map.setCenter(denver);
+});
+
+$('#la').on('click', function() {
+    map.setZoom(8);
+    map.setCenter(la);
 });
 
 $('#newyork').on('click', function() {
@@ -105,3 +123,28 @@ $('#london').on('click', function() {
     map.setZoom(8);
     map.setCenter(london);
 });
+
+$('#worldview').on('click', function() {
+    map.setZoom(2);
+    map.setCenter(worldview);
+});
+
+
+function initMap() {
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: {
+            lat: 40.674,
+            lng: -104.945,
+        },
+        zoom: 5,
+    });
+
+} // end map function
+
+function clearCircles() {
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].setMap(null)
+    }
+    circles = [];
+}
--- play.js
+++ play.js
@@ -140,7 +140,78 @@
 //     }
 
 
-//EVENT INFO? 
+//EVENT INFO?
 //if data.ticket_url >> display event info on hover
 //connect with bubble
 // }
+
+
+//WORKING BUTTON FUNCTION
+// $('button').click(function() {
+//
+//         var artist = $('input').val().toLowerCase();
+//         $('input').val('');
+//
+//         // var events = [];
+//
+//         $.get('https://galvanize-cors-proxy.herokuapp.com/http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=musicmap&date=all')
+//
+//         .then(function(data) {
+//             var cityList = {}
+//             for (var j = 0; j < data.length; j++) {
+//                 var city = data[j].venue.city;
+//
+//                 if (!cityList[city]) {
+//                     cityList[city] = {
+//                         center: {
+//                             lat: data[j].venue.latitude,
+//                             lng: data[j].venue.longitude
+//                         },
+//                         count: 1,
+//                     };
+//                 } else {
+//                     cityList[city].count++
+//                 } // end city count
+//             }
+//
+//             // console.log(cityList);
+//
+//             for (var city in cityList) {
+//                 // Add the circle for this city to the map.
+//                 var cityCircle = new google.maps.Circle({
+//                     strokeColor: '#FF0000',
+//                     strokeOpacity: 0.8,
+//                     strokeWeight: 2,
+//                     fillColor: '#FF0000',
+//                     fillOpacity: 0.35,
+//                     map: map,
+//                     center: cityList[city].center,
+//                     radius: Math.sqrt(cityList[city].count) * 4000
+//                 });
+//             } // end circle add
+//
+//             $('#err').hide();
+//
+//         }).catch(function(error) {
+//             $('#err').show();
+//         })
+//     }) // end button click function
+
+
+
+//CLICK OVER?
+//   google.maps.event.addListener(marker, 'mouseover', function() {
+//   	infowindow.open(map, this);
+//   });
+// //  And then to make the infowindow disappear again when you move the mouse away from the marker, just call the close() function on mouseout.
+//
+//   google.maps.event.addListener(marker, 'mouseout', function() {
+//   	infowindow.close();
+//   }); OR
+
+
+// var infowindow = "some shit";
+//
+// cityCircle.addListener('click', function() {
+//     infowindow.open(map, cityCircle);
+// });
